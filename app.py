009dc288b39ff86c66e59ad85291902ee2503419
Hassan Akhavanpour <hakhavanpour82@gmail.com>
2026-06-09 21:59:01 +0200

    chore: corrected python code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import time
 
 print("--- Traditional Deployment Era ---")
-print("Sallam Dadash! In kod bedone Docker va ghadimi ruan shode. íº€")
+print("Sallam Dadash! In kod bedone Docker va ghadimi ruan shode.")
 print("Time on Server:", time.strftime("%Y-%m-%d %H:%M:%S"))
 
