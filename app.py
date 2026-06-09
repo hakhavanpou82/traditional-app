38adab7ea6c52b4542aa27fddf9d06e3a0431b48
Hassan Akhavanpour <hakhavanpour82@gmail.com>
2026-06-09 21:02:27 +0200

    chore: initial traditional python script

---
+++ app.py
@@ -0,0 +1,6 @@
+import time
+
+print("--- Traditional Deployment Era ---")
+print("Sallam Dadash! In kod bedone Docker va ghadimi ruan shode. íº€")
+print("Time on Server:", time.strftime("%Y-%m-%d %H:%M:%S"))
+
